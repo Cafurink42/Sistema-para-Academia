5ac27e40c7d30d2465138d7c5af3fa9bfee9633e
Daniel <carlosdanielcotticaguth@gmail.com>
2023-11-15 19:53:44 -0300

    Funcionalidade de exclusão de cadastro e clean code

--- Cliente/excluir.php
+++ Cliente/excluir.php
@@ -35,6 +35,26 @@
     </div>
   </nav>
 
+  <?php
+  include ('../conexao.php');
+  
+  if ($_SERVER["REQUEST_METHOD"] == "GET") {
+      $id = $_GET['id'];
+  
+      $sql = "DELETE FROM cliente WHERE id=$id";
+      if ($conexao->query($sql) === TRUE) {
+  
+          header("Location: listar.php");
+  
+      } else {
+          echo "Erro ao deletar cliente: " . $conexao->error;
+      }
+  }
+  
+  $conexao->close();
+  
+  ?>
+
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
