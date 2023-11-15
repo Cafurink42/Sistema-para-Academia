cd5a667eacaf48b451b65667f3aafaff7a714f2d
Daniel <carlosdanielcotticaguth@gmail.com>
2023-11-15 20:25:11 -0300

    Colocando margin nos links de acesso Inicio e Cliente

--- Cliente/listar.php
+++ Cliente/listar.php
@@ -32,8 +32,8 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.9.0/dist/sweetalert2.min.css
     <br>
 
     <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item ms-3" ><a href="../index.html">Início</a></li>
+        <ol class="breadcrumb ms-3">
+            <li class="breadcrumb-item " ><a href="../index.html">Início</a></li>
             <li class="breadcrumb-item active" aria-current="page">Cliente</li>
         </ol>
     </nav>
